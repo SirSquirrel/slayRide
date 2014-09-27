d841f529dcaf8e7a0893ced6ed2c3c26c558187d
SquirrelKnight <ksquires11@gmail.com>
2014-09-27 11:17:44 -0700

    uploading assets

--- gameJam2014/Assets/puller_control.cs
+++ gameJam2014/Assets/puller_control.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class puller_control : MonoBehaviour {
 
+	public float maxSpeed = 10f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float xMove = Input.GetAxis("Horizontal");
+		float yMove = Input.GetAxis("Vertical");
+
+		//change velocity
+		rigidbody2D.velocity = new Vector2(xMove * maxSpeed, yMove * maxSpeed);
+
 	}
 }
