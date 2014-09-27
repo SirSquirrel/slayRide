f694e664afbab555e034d0eea7b217ec9920ff2f
SquirrelKnight <ksquires11@gmail.com>
2014-09-27 13:45:53 -0700

    fixing error in enemy behaviour

--- gameJam2014/Assets/enemyBehaviour.cs
+++ gameJam2014/Assets/enemyBehaviour.cs
@@ -1,25 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class enemyBehaviour : MonoBehaviour {
-	//enemy that moves to player when close enough
+public class enemyBehaviour : MonoBehaviour
+{
+    //enemy that moves to player when close enough
 
-	public float speed = 10f;
-	public Transform player;
-	float distance_to_player;
-	public float enemySight = 500f;
+    public float speed = 10f;
+    public Transform player;
+    public float enemySight = 1000f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		distance_to_player = new Vector2(player.position,rigidbody2D.position);
+    // Use this for initialization
+    void Start()
+    {
 
-			
-			Vector2.MoveTowards(rigidbody2D.position,player.position,enemySight);
-			
-	}
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        var heading = player.position - transform.position;
+
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+
+        if (heading.sqrMagnitude < enemySight * enemySight)
+        {
+
+        }
+
+    }
 }
